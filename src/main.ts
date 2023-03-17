d7d222034078ea2c8a36501a41d93811e123bc5e
Julien Von Der Marck <vdmjulien05@gmail.com>
2023-03-17 09:31:16 +0100

    feat: :sparkles: create database service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DatabaseService } from './services/database/database.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const prismaService = app.get(DatabaseService);
+  await prismaService.enableShutdownHooks(app);
   await app.listen(3000);
 }
 bootstrap();
