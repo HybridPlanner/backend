a805e7620f37ab75fcbbaf1f134f7053c4728937
Louis Bailleau <louis.bailleau@free.fr>
2023-03-17 10:01:36 +0100

    ci: 💚 fix configurations to make the CI build
    close #4

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,9 +17,10 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    '@typescript-eslint/explicit-member-accessibility': 'error',
     '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'error',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
   },
 };
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DatabaseService } from './services/database/database.service';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   const prismaService = app.get(DatabaseService);
   await prismaService.enableShutdownHooks(app);
