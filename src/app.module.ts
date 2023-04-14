9a3780d4fef6a2ad63e63fbcf1f048fd995b6eb9
Samy Mosa <samymosa02@gmail.com>
2023-04-14 19:32:44 +0200

    feat: :sparkles: Add MeetingScheduleService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,23 @@ import { AppController } from './app.controller';
 import { DatabaseService } from './services/database/database.service';
 import { MailModule } from './mail/mail.module';
 import { MailService } from './mail/mail.service';
+import { ScheduleModule } from '@nestjs/schedule';
+import { MeetingScheduleService } from './meeting-schedule/meeting-schedule.service';
 
 @Module({
-  imports: [MailModule],
+  imports: [MailModule, ScheduleModule.forRoot()],
   controllers: [AppController],
-  providers: [DatabaseService],
+  providers: [DatabaseService, MeetingScheduleService],
 })
 export class AppModule {
-  // send email
-  public constructor(private mailService: MailService) {
-    this.mailService.sendUserConfirmation();
+  public constructor(
+    private mailService: MailService,
+    private meetingScheduleService: MeetingScheduleService,
+  ) {
+    const inOneMinute = new Date(Date.now() + 60 * 1000);
+    this.meetingScheduleService.scheduleMail(
+      inOneMinute,
+      'hello.world@gmail.com',
+    );
   }
 }
