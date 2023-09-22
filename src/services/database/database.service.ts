6f75ddf3cf76e26c692e059577ea08be9bb8e89b
Louis Bailleau <louis.bailleau@free.fr>
2023-09-22 11:50:37 +0200

    fix: app not building after dependencies update

--- src/services/database/database.service.ts
+++ src/services/database/database.service.ts
@@ -1,15 +1,9 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class DatabaseService extends PrismaClient implements OnModuleInit {
   public async onModuleInit(): Promise<void> {
     await this.$connect();
-  }
-
-  public async enableShutdownHooks(app: INestApplication): Promise<void> {
-    this.$on('beforeExit', async () => {
-      await app.close();
-    });
   }
 }
