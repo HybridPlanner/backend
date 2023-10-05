a115fad1aaf66210861b5af4e01ebe7aecda6482
Louis Bailleau <louis.bailleau@free.fr>
2023-10-05 09:53:41 +0200

    fix: wrong fields for meeting dto object

--- src/meetings/meetings.service.ts
+++ src/meetings/meetings.service.ts
@@ -24,7 +24,7 @@ export class MeetingsService {
     return this.database.meeting.findMany({
       take: limit,
       where: {
-        date: {
+        start_date: {
           lte: previous,
         },
       },
