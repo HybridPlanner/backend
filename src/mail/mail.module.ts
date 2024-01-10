568f8bea2eb351b11cbe48eeaea749a78b8ca300
Samy MOSA <samymosa02@gmail.com>
2024-01-10 22:08:27 +0100

    fix: :wrench: Make mailer configurable with .env

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -4,30 +4,45 @@ import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { join } from 'path';
 import { IcsService } from 'src/ics/ics.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EnvConfig } from '../config';
 
 /**
  * Module for handling email functionality.
  */
 @Module({
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        host: process.env.MAILHOG_HOST || 'mailhog',
-        port: 1025,
-        secure: false,
-        debug: true,
-        logger: true,
-      },
-      defaults: {
-        from: '"No Reply" <noreply@example.com>',
-      },
-      template: {
-        dir: join(__dirname, 'templates'),
-        adapter: new HandlebarsAdapter(),
-        options: {
-          strict: true,
+    MailerModule.forRootAsync({
+      inject: [ConfigService<EnvConfig>],
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService<EnvConfig>) => ({
+        transport: {
+          host: config.get<string>('MAIL_HOST'),
+          port: config.get<number>('MAIL_PORT'),
+          secure: false,
+          debug: true,
+          logger: true,
+
+          // Add auth only if user and pass are defined
+          ...(config.get<string>('MAIL_USER') &&
+            config.get<string>('MAIL_PASS') && {
+              auth: {
+                user: config.get<string>('MAIL_USER'),
+                pass: config.get<string>('MAIL_PASS'),
+              },
+            }),
         },
-      },
+        defaults: {
+          from: config.get<string>('MAIL_DEFAULT_FROM'),
+        },
+        template: {
+          dir: join(__dirname, 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
     }),
   ],
   providers: [MailService, IcsService],
